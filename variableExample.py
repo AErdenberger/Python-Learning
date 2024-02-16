959c2528068012c549155a8816ab1cd85720479d
AErdenberger <erdenberger.alexander@gmail.com>
2024-02-16 12:34:07 -0800

    looking a lot like the last one, oops

---
+++ variableExample.py
@@ -0,0 +1,16 @@
+#variables just need a name and a value
+#types are interpreted
+#you CAN explicity make a type if you want
+
+#best practice to use snake_case with variables in Python
+is_game_over = False
+print(is_game_over)
+#expected False
+
+is_game_over = True
+print(is_game_over)
+#expected True
+
+is_game_over = 5 > 6
+print(is_game_over)
+#expected False
