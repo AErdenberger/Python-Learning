0124018523e19f842b8e209685c78d84a5b17c8c
AErdenberger <erdenberger.alexander@gmail.com>
2024-02-15 18:37:21 -0800

    simple returns

--- Learning.py
+++ Learning.py
@@ -1,2 +1,24 @@
 #initial commit to this document
 #git is being weird and I just want to make sure it's working
+
+
+is_game_over = False
+print(is_game_over)
+
+is_game_over = True
+print(is_game_over)
+
+is_game_over = 5 > 6
+print(is_game_over)
+
+#false
+#true
+#false
+
+num_lives = 5
+print(num_lives)
+percent_health = 0.5
+print(percent_health)
+
+#5
+#0.5
