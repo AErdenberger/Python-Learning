2a477ad9946cfd9339efd50769d7882207248201
AErdenberger <erdenberger.alexander@gmail.com>
2024-02-16 13:09:56 -0800

    arithmatic operators

---
+++ Operators.py
@@ -0,0 +1,24 @@
+is_game_over = False
+num_lives = 5
+percent_health = 0.5
+player_name = 'Mammoth'
+
+#Arithmatic Operators
+health = 50
+health = health + 20
+new_health = health + 20
+#one is modifying the base value of health and assigning it ot 70
+#the other is storing the result in a new variable
+
+xPos = 5
+print(xPos % 2)
+print(xPos // 2)
+# % will return the remainder
+# // will discard the remainder
+
+#expected
+#1
+#2
+
+
+#Assignment Operators
